dbea139728a62a3829bfa1557118d769c2529e3d
Kai <kai@kaiherzig.com>
2021-03-09 09:48:37 +0100

    Initial

---
+++ get-delegated-ad-permission.ps1
@@ -0,0 +1,46 @@
+$ViewMode = "grid" #Grid oder csv
+$DomainController = "mydc.mydomain.com"
+$DN = "DC=mydomain,DC=com"
+#If using CSV specify FilePath
+$FilePath = "C:\Users\Administrator\Desktop\query.csv"
+
+$ObjectFilter = "(|(objectClass=domain)(objectClass=organizationalUnit)(objectClass=group)(sAMAccountType=805306368)(objectCategory=Computer))"
+
+$baseSearch = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$DomainController/$DN")
+$dnSearch = New-Object System.DirectoryServices.DirectorySearcher($bSearch)
+$dnSearch.SearchRoot = $baseSearch
+$dnSearch.PageSize = 1000
+#$dSearch.Filter = $ObjectFilter
+$dnSearch.SearchScope = "Subtree"
+
+$extPerms = `
+        '00299570-246d-11d0-a768-00aa006e0529',
+        'ab721a54-1e2f-11d0-9819-00aa0040529b',
+        '0'
+
+$result = @()
+
+foreach ($objResult in $dSearch.FindAll())
+{
+    $obj = $objResult.GetDirectoryEntry()
+
+    Write-Host "Searching... " $obj.distinguishedName
+
+    $permissions = $obj.PsBase.ObjectSecurity.GetAccessRules($true,$false,[Security.Principal.NTAccount])
+    
+    $result += $permissions | Where-Object { `
+            $_.AccessControlType -eq 'Allow' -and ($_.ObjectType -in $extPerms) -and $_.IdentityReference -notin ('NT AUTHORITY\SELF', 'NT AUTHORITY\SYSTEM', 'S-1-5-32-548') `
+            } | Select-Object `
+        @{n='Object'; e={$obj.distinguishedName}}, 
+        @{n='Account'; e={$_.IdentityReference}},
+        @{n='Permission'; e={$_.ActiveDirectoryRights}}
+
+}
+if($ViewMode -eq "grid")
+{
+$result | Out-GridView
+}
+if($ViewMode -eq "csv")
+{
+$result | Export-CSV $FilePath
+}
